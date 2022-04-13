35a212cf3f5d5c16fbf01357a42a607d64df0c91
PranavKumbhar <pranavkumbhar7@gmail.com>
2022-04-13 16:33:29 +0530

    first commit

---
+++ src/main/java/Multiplex/seat/booking/System/MultiplexSeatBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package Multiplex.seat.booking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplexSeatBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplexSeatBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Multiplex/seat/booking/System/MultiplexSeatBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Multiplex.seat.booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplexSeatBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
